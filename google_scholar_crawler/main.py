37605db0a9504ad91e5d75ceca77402d183c4d89
Manish Munikar <munikarmanish@gmail.com>
2025-05-25 10:37:32 -0500

    fixed error in get_citation_data

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -1,19 +1,32 @@
-from scholarly import scholarly
-import jsonpickle
 import json
-from datetime import datetime
 import os
+from datetime import datetime
 
-author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+import jsonpickle
+from scholarly import ProxyGenerator, scholarly
+
+SCHOLAR_ID = os.environ['GOOGLE_SCHOLAR_ID']
+
+pg = ProxyGenerator()
+pg.FreeProxies()
+scholarly.use_proxy(pg)
+
+print(f"Fetching author info with ID={SCHOLAR_ID}")
+author = scholarly.search_author_id(SCHOLAR_ID)
+print("Author found!")
+
+print("Filling extra info about the author...")
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
-name = author['name']
 author['updated'] = str(datetime.now())
 author['publications'] = {v['author_pub_id']:v for v in author['publications']}
-print(json.dumps(author, indent=2))
+#print(json.dumps(author, indent=2))
+
+print("Writing gs_data.json...")
 os.makedirs('results', exist_ok=True)
 with open(f'results/gs_data.json', 'w') as outfile:
     json.dump(author, outfile, ensure_ascii=False)
 
+print("Writing gs_data_shieldsio.json...")
 shieldio_data = {
   "schemaVersion": 1,
   "label": "citations",
@@ -21,3 +34,5 @@ shieldio_data = {
 }
 with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
     json.dump(shieldio_data, outfile, ensure_ascii=False)
+
+print("Done!")
